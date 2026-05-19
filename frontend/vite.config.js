2886b2ff2bedd055d9089fbec16b6f5e7533ed4d
超 <3174753262@qq.com>
2026-05-19 21:38:01 +0800

    fix: 部署修改

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,8 +8,9 @@ export default defineConfig({
     proxy: {
       '/api': {
         // target: 'http://localhost:8080',
-        target: 'https://couple-order.onrender.com/api',
-        changeOrigin: true
+        target: 'https://couple-order.onrender.com',
+        changeOrigin: true,
+        timeout: 120000
       }
     }
   }
